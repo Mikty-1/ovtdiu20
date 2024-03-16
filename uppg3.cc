d93e92b857d29f3cce47a83b2ca9c0a89819234e
Mikty-1 <153192655+Mikty-1@users.noreply.github.com>
2024-03-16 17:48:26 +0100

    Klar

--- uppg3.cc
+++ uppg3.cc
@@ -6,69 +6,105 @@
 #include <stdexcept>
 class Stack{
 public:
-  Stack()
-    : head{nullptr}
-  {
-
-  }
-
-  ~Stack()
-  {
-    while (head !=nullptr)
+    Stack()
+        : head{nullptr}
     {
-      pop();
-    }
-  }
 
-  Stack(Stack const& other)
-    : head {}
-  {
-    Node* curr = other.head;
-    while(curr != nullptr)
-    {
-      int tmp = curr->value;
-      push(tmp);
-      curr = curr->next;
     }
 
-  }
+    ~Stack()
+    {
+        delete_all();
+    }
 
-  void push(int value)
-  {
-    head = new Node {value, head};
-  }
+    Stack(Stack const& other)
+        : head {}
+    {
+        Stack s1 {};
+        s1.clone(other);
+        
+        clone(s1);
+    }
+
+    Stack& operator=(Stack const& other)
+    {
+        delete_all();
+
+        Stack s1 {};
+        s1.clone(other);
+        clone(s1);
+
+        return *this;
+    }
+
+    Stack(Stack && other)
+        : head{other.head}
+    {
+        other.head = nullptr;
+    }
+
+    Stack& operator=(Stack && other)
+    {
+        std::swap(head, other.head);
+        return *this;
+    }
     
-  void pop()
-  {
-    if(head != nullptr)
+    void push(int value)
     {
-      Node* popped {head};
-      head = head->next;
-      delete popped;
+        head = new Node {value, head};
     }
-  }
-
-  int top()
-  {
-    if(head != nullptr)
+    
+    void pop()
     {
-      return head->value;
+        if(head != nullptr)
+        {
+            Node* popped {head};
+            head = head->next;
+            delete popped;
+        }
     }
-    throw std::runtime_error{"List Empty in top()"};
-  }
 
-  bool empty()
-  {
-    return head == nullptr;
-  }
+    int top()
+    {
+        if(head != nullptr)
+        {
+            return head->value;
+        }
+        throw std::runtime_error{"List Empty in top()"};
+    }
+
+    bool empty()
+    {
+        return head == nullptr;
+    }
 private:
-  struct Node
-  {
-    int value;
-    Node* next;
-  };
 
-  Node* head;
+    void clone(Stack const& other)
+    {
+        Node* curr = other.head;
+        while(curr != nullptr)
+        {
+            int tmp = curr->value;
+            push(tmp);
+            curr = curr->next;
+        }
+    }
+
+    void delete_all()
+    {
+        while (head !=nullptr)
+        {
+            pop();
+        }
+    }
+    
+    struct Node
+    {
+        int value;
+        Node* next;
+    };
+
+    Node* head;
 
 };
 
@@ -78,27 +114,64 @@ private:
 
 TEST_CASE("all")
 {
+    //1
     Stack A{};
     A.push(2);
     A.push(5);
     A.push(8);
     CHECK( ! A.empty() );
     CHECK( A.top() == 8 );
-
+    //2
     Stack B{A};
     CHECK( ! B.empty() );
     CHECK( B.top() == 8 );
-    
+    //3
     B.pop();
     B.pop();
     CHECK( B.top() == 2);
+    //4
     B.push(4);
     CHECK(! B.empty());
     CHECK(B.top()== 4);
+    //5
     A.pop();
     CHECK(! A.empty()); 
     CHECK( A.top() == 5);
-
+    //6
+    A.pop();
+    A.pop();
+    CHECK(A.empty());
+    //7
+    A = std::move(B);
+    CHECK(B.empty());
+    CHECK(! A.empty());
+    CHECK(A.top() == 4);
+    //8
+    Stack C{std::move(A)};
+    CHECK(A.empty());
+    CHECK(! C.empty());
+    CHECK(C.top() == 4);
+    //9
+    Stack D {};
+    D.push(3);
+    CHECK(! D.empty());
+    CHECK( D.top() == 3);
+    //10
+    C = D;
+    CHECK(! C.empty());
+    CHECK( C.top() == 3);
+    //11
+    C.pop();
+    CHECK(C.empty());
+    CHECK(! D.empty());
+    CHECK(D.top() == 3);
+    //12
+    D.push(1);
+    D.push(2);
+    D.push(6);
+    D.push(7);
+    
+    
     // Lägg till testfall 3-12 här!
 
     // Glöm inte kontroll av minnesläckor med valgrind!
